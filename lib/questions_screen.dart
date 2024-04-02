5684535ce1cf003aec4183c5dae44b144a3623ee
sam-kash <kashyapsankalpa@gmail.com>
2024-04-02 23:23:42 +0530

    Alignment, margin , padding

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -21,28 +21,33 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment
-            .center, // Literally bring the things in the center of the screen
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-                color: Color.fromARGB(255, 146, 176, 222), fontSize: 18),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment
+              .center, // Literally bring the things in the center of the screen VERTICAL AXIS
+          crossAxisAlignment: CrossAxisAlignment.stretch, // Horizontal axis
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                  color: Color.fromARGB(255, 146, 176, 222), fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
 
-          // Dynamically generated instead of hardcoded
+            // Dynamically generated instead of hardcoded
 
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(
-              answerText: answer,
-              onTap: () {},
-            );
-          })
-        ],
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {},
+              );
+            })
+          ],
+        ),
       ),
     );
   }
